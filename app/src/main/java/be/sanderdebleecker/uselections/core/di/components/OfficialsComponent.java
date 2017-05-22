047443850d6f7ba6646ccb5750ec0c8271236bf2
simulan <simulan88@yahoo.com>
2017-05-22 13:30:58 +0200

    added mvp to OfficalsActivity

---
+++ app/src/main/java/be/sanderdebleecker/uselections/core/di/components/OfficialsComponent.java
@@ -0,0 +1,10 @@
+package be.sanderdebleecker.uselections.core.di.components;
+
+/**
+ * @author Sander De Bleecker
+ * @version 1.0.0
+ * @since 22/05/2017
+ */
+
+public interface OfficialsComponent {
+}
---
+++ app/src/main/java/be/sanderdebleecker/uselections/mvp/view/OfficialsView.java
@@ -0,0 +1,10 @@
+package be.sanderdebleecker.uselections.mvp.view;
+
+/**
+ * @author Sander De Bleecker
+ * @version 1.0.0
+ * @since 22/05/2017
+ */
+
+public class OfficialsView {
+}
